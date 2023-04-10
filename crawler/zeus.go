6f8fdbd2381ca3ef3bee717b09918fff1d2931f8
GaosongXu <shimaring@qq.com>
2023-04-10 20:23:41 +0800

    feat: finish zeus algorithm

--- crawler/zeus.go
+++ crawler/zeus.go
@@ -1,14 +1,32 @@
 package crawler
 
 import (
+	"github.com/ethereum/go-ethereum/p2p/discover"
 	"github.com/ethereum/go-ethereum/p2p/enode"
 	_ "unsafe"
 )
+
+const MAX_LENGTH = 255
+const lookupRequestLimit int = 3
+
+var ALL_ZERO enode.ID
+var ALL_ONES enode.ID
+
+func init() {
+	for i := 0; i < 32; i++ {
+		ALL_ZERO[i] = 0
+		ALL_ONES[i] = 255
+	}
+}
 
 //store the from and the end of enode id
 type pair struct {
 	from enode.ID
 	end  enode.ID
+}
+
+func newPair(from enode.ID, end enode.ID) *pair {
+	return &pair{from: from, end: end}
 }
 
 type queue struct {
@@ -70,7 +88,89 @@ func getClosetKey(arr []*enode.Node, target enode.ID) *enode.Node {
 	return closet
 }
 
-//TODO: implementation of the zeus algorithm
+//if a>b return 1 else if a<b return -1 else return 0
+func compareId(a, b enode.ID) int {
+	for i := 0; i < len(a); i++ {
+		if a[i] > b[i] {
+			return 1
+		} else if a[i] < b[i] {
+			return -1
+		}
+	}
+	return 0
+}
+
+//0~255
+//return c and the length of c ,the length of c is 0~255
+//we need to get the common prefix of a and b
+//we will return common prefix of a and b and the length of common prefix
+//the length of common prefix is 0~255
+func getCommonPrefix(a, b enode.ID) int {
+	var length int
+	for i := 0; i < 32; i++ {
+		if a[i] == b[i] {
+			length += 8
+			continue
+		}
+		var at = a[i]
+		var bt = b[i]
+		for j := 7; j >= 0; j-- {
+			if (at>>j)&1 == (bt>>j)&1 {
+				length++
+			} else {
+				break
+			}
+		}
+		break
+	}
+	return length
+}
+
+//length bits start with 0 and other bits are 1
+func mergeWith01(a enode.ID, length int) enode.ID {
+	if length == MAX_LENGTH {
+		return a
+	}
+	//set the length+1 bit with after to 1 ,and the length bit to 0
+	//we need to get the length/8 byte
+	var byteIndex = length / 8
+	//we need to get the length%8 bit
+	var bitIndex = length % 8
+	var count = a[byteIndex]
+	//set the count
+	count = count & ^(1 << (7 - bitIndex))
+	//set the length+ 1 bit and other to 1
+	mask := (1 << (7 - bitIndex)) - 1
+	count = count | mask
+	for i := byteIndex + 1; i < len(a); i++ {
+		a[i] = 255
+	}
+	var b = enode.ID{}
+	copy(b[:], a[:])
+	return b
+}
+
+func mergeWith10(a enode.ID, length int) enode.ID {
+	if length == MAX_LENGTH {
+		return a
+	}
+	//set the length+1 bit with after to 0 ,and the length bit to 1
+	//we need to get the length/8 byte
+	var byteIndex = length / 8
+	//we need to get the length%8 bit
+	var bitIndex = length % 8
+	var count = a[byteIndex]
+	//set the count
+	count = count | (1 << (7 - bitIndex))
+	//set the length+ 1 bit and other to 1
+	mask := ^((1 << (7 - bitIndex)) - 1)
+	count = count & mask
+	for i := byteIndex + 1; i < len(a); i++ {
+		a[i] = 0
+	}
+	return a
+}
+
 func (c *Crawler) crawlZeus(node *enode.Node) ([]*enode.Node, error) {
 	conn, err := c.discv5Pool.Get()
 	if err != nil {
@@ -88,5 +188,80 @@ func (c *Crawler) crawlZeus(node *enode.Node) ([]*enode.Node, error) {
 	*node = *conn.Conn.Resolve(node)
 	//256 bits
 
-	return nil, nil
+	q := newQueue()
+
+	l := set(make(map[enode.ID]*enode.Node))
+	m, err := requestL(conn.Conn, node, ALL_ZERO)
+	if err != nil {
+		return nil, err
+	}
+
+	l.merge(m)
+	k_first := getClosetKey(m, ALL_ZERO)
+	m, err = requestL(conn.Conn, node, ALL_ONES)
+	if err != nil {
+		return nil, err
+	}
+	l.merge(m)
+	k_last := getClosetKey(m, ALL_ONES)
+	if compareId(k_first.ID(), k_last.ID()) != 0 {
+		q.push(newPair(k_first.ID(), k_last.ID()))
+	}
+	for !q.empty() {
+		p := q.pop()
+		k1 := p.from
+		k2 := p.end
+		length := getCommonPrefix(k1, k2)
+		s1 := mergeWith01(k1, length)
+		s2 := mergeWith10(k2, length)
+		if compareId(k1, s1) < 0 {
+			m, err = requestL(conn.Conn, node, s1)
+			if err != nil {
+				return nil, err
+			}
+			l.merge(m)
+			x := getClosetKey(m, s1)
+			if compareId(x.ID(), k1) != 0 {
+				q.push(newPair(k1, x.ID()))
+			}
+		}
+		if compareId(k2, s2) > 0 {
+			m, err = requestL(conn.Conn, node, s2)
+			if err != nil {
+				return nil, err
+			}
+			l.merge(m)
+			y := getClosetKey(m, s2)
+			if compareId(y.ID(), k2) != 0 {
+				q.push(newPair(y.ID(), k2))
+			}
+		}
+	}
+	var result []*enode.Node
+	for _, v := range l {
+		result = append(result, v)
+	}
+	return result, nil
+}
+
+func requestL(dis *discover.UDPv5, destNode *enode.Node, target enode.ID) ([]*enode.Node, error) {
+	dists := lookupDistances(target, destNode.ID())
+	return findnode(dis, destNode, dists)
+}
+
+//go:linkname findNode github.com/ethereum/go-ethereum/p2p/discover/t.(*UDPv5).findnode
+func findnode(_ *discover.UDPv5, n *enode.Node, distances []uint) ([]*enode.Node, error)
+
+func lookupDistances(target, dest enode.ID) (dists []uint) {
+	td := enode.LogDist(target, dest)
+	dists = append(dists, uint(td))
+	for i := 1; len(dists) < lookupRequestLimit; i++ {
+		if td+i < 256 {
+			dists = append(dists, uint(td+i))
+		}
+		if td-i > 0 {
+			dists = append(dists, uint(td-i))
+		}
+	}
+	return dists
 }
